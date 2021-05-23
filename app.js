5341c409c3652116d530e7a300046380e8ad7fd8
Your Name <you@example.com>
2021-05-23 08:01:27 +0700

    fix cors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(passport.initialize());
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
